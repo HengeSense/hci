3d5d3c35996c344f580587b24f623eb1be4fb0a3
Arthur Pang <apang42@gmail.com>
2012-03-10 12:45:31 -0700

    fixed bug. now we serve html pages for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,5 @@ class User < ActiveRecord::Base
   
   has_many :bills, :class_name => 'Transaction', :foreign_key => :sender_id
   has_many :invoices, :class_name => 'Transaction', :foreign_key => :recipient_id
+  
 end
