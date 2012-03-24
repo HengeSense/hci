ec622826b1e62b8a2328352f32268d3966d16232
Arthur Pang <apang42@gmail.com>
2012-03-23 19:24:13 -0700

    fixed production bug

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -13,20 +13,20 @@ class UserMailer < ActionMailer::Base
     mail(:to => "#{recipientEmail}", :subject => "#{sender.email} sent you $#{transaction.amount}!")
   end
   
-  class Preview < MailView
-    # Pull data from existing fixtures
-    def registration_confirmation
-      user = User.first
-      UserMailer.registration_confirmation(user)
-    end
-
-    # Factory-like pattern
-    def registration_invitation
-      user = User.first
-      transaction = Transaction.first
-      mail = UserMailer.registration_invitation('bob@example.com', user, transaction)
-      mail
-    end
-  end
+  # class Preview < MailView
+  #   # Pull data from existing fixtures
+  #   def registration_confirmation
+  #     user = User.first
+  #     UserMailer.registration_confirmation(user)
+  #   end
+  # 
+  #   # Factory-like pattern
+  #   def registration_invitation
+  #     user = User.first
+  #     transaction = Transaction.first
+  #     mail = UserMailer.registration_invitation('bob@example.com', user, transaction)
+  #     mail
+  #   end
+  # end
   
 end
