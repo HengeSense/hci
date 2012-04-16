1588530099ff06f6c3cf12ff2e3ea6a45f512ec5
Arthur Pang <apang42@gmail.com>
2012-04-16 13:54:49 -0700

    more logger stuff

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -63,7 +63,7 @@ class TransactionsController < ApplicationController
         format.html { redirect_to @transaction, notice: 'Invoice successfully sent!' }
         format.json { render json: @transaction, status: :created, location: @transaction }
       else
-        logger @transaction.errors
+        logger.info @transaction.errors
         format.html { render action: "new" }
         format.json { render json: @transaction.errors, status: :unprocessable_entity }
       end
