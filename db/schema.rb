300a42ff197bbe1bb84b4495dfa8b953cf3bd03b
Joshua Conner <joshuaconner@nau.edu>
2012-04-18 03:48:54 -0700

    Adding 'description' field to Users

--- db/schema.rb
+++ db/schema.rb
@@ -11,73 +11,94 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120416112532) do
+ActiveRecord::Schema.define(:version => 20120418104653) do
 
   create_table "items", :force => true do |t|
-    t.string   "name"
-    t.string   "description"
-    t.integer  "merchant_id"
-    t.integer  "buyer_id"
-    t.datetime "created_at",         :null => false
-    t.datetime "updated_at",         :null => false
-    t.string   "photo_file_name"
-    t.string   "photo_content_type"
-    t.integer  "photo_file_size"
-    t.datetime "photo_updated_at"
-    t.integer  "transaction_id"
-    t.integer  "purchase_price"
-    t.string   "currency"
+    t.string    "name"
+    t.string    "description"
+    t.integer   "merchant_id"
+    t.integer   "buyer_id"
+    t.timestamp "created_at",         :null => false
+    t.timestamp "updated_at",         :null => false
+    t.string    "photo_file_name"
+    t.string    "photo_content_type"
+    t.integer   "photo_file_size"
+    t.timestamp "photo_updated_at"
+    t.integer   "transaction_id"
+    t.integer   "purchase_price"
+    t.string    "currency"
   end
 
+  create_table "merchants", :force => true do |t|
+    t.string    "email",                  :default => "", :null => false
+    t.string    "encrypted_password",     :default => "", :null => false
+    t.string    "reset_password_token"
+    t.timestamp "reset_password_sent_at"
+    t.timestamp "remember_created_at"
+    t.integer   "sign_in_count",          :default => 0
+    t.timestamp "current_sign_in_at"
+    t.timestamp "last_sign_in_at"
+    t.string    "current_sign_in_ip"
+    t.string    "last_sign_in_ip"
+    t.timestamp "created_at",                             :null => false
+    t.timestamp "updated_at",                             :null => false
+    t.string    "name"
+    t.decimal   "balance"
+  end
+
+  add_index "merchants", ["email"], :name => "index_merchants_on_email", :unique => true
+  add_index "merchants", ["reset_password_token"], :name => "index_merchants_on_reset_password_token", :unique => true
+
   create_table "price_points", :force => true do |t|
-    t.integer  "price_cents"
-    t.string   "currency"
-    t.integer  "item_id"
-    t.string   "name"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.integer   "price_cents"
+    t.string    "currency"
+    t.integer   "item_id"
+    t.string    "name"
+    t.timestamp "created_at",  :null => false
+    t.timestamp "updated_at",  :null => false
   end
 
   create_table "transactions", :force => true do |t|
-    t.string   "sender_email"
-    t.string   "recipient_email"
-    t.boolean  "complete"
-    t.string   "description"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
-    t.integer  "sender_id"
-    t.integer  "recipient_id"
-    t.integer  "amount"
-    t.string   "currency"
+    t.string    "sender_email"
+    t.string    "recipient_email"
+    t.boolean   "complete"
+    t.string    "description"
+    t.timestamp "created_at",      :null => false
+    t.timestamp "updated_at",      :null => false
+    t.integer   "sender_id"
+    t.integer   "recipient_id"
+    t.integer   "amount"
+    t.string    "currency"
   end
 
   add_index "transactions", ["recipient_email"], :name => "index_transactions_on_recipient_email"
   add_index "transactions", ["sender_email"], :name => "index_transactions_on_sender_email"
 
   create_table "users", :force => true do |t|
-    t.string   "email",                  :default => "",    :null => false
-    t.string   "encrypted_password",     :default => "",    :null => false
-    t.string   "name",                   :default => "",    :null => false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          :default => 0
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at",                                :null => false
-    t.datetime "updated_at",                                :null => false
-    t.string   "avatar_file_name"
-    t.string   "avatar_content_type"
-    t.integer  "avatar_file_size"
-    t.datetime "avatar_updated_at"
-    t.integer  "balance"
-    t.string   "currency"
-    t.string   "address"
-    t.float    "latitude"
-    t.float    "longitude"
-    t.boolean  "is_merchant",            :default => false
+    t.string    "email",                  :default => "",    :null => false
+    t.string    "encrypted_password",     :default => "",    :null => false
+    t.string    "name",                   :default => "",    :null => false
+    t.string    "reset_password_token"
+    t.timestamp "reset_password_sent_at"
+    t.timestamp "remember_created_at"
+    t.integer   "sign_in_count",          :default => 0
+    t.timestamp "current_sign_in_at"
+    t.timestamp "last_sign_in_at"
+    t.string    "current_sign_in_ip"
+    t.string    "last_sign_in_ip"
+    t.timestamp "created_at",                                :null => false
+    t.timestamp "updated_at",                                :null => false
+    t.string    "avatar_file_name"
+    t.string    "avatar_content_type"
+    t.integer   "avatar_file_size"
+    t.timestamp "avatar_updated_at"
+    t.integer   "balance"
+    t.string    "currency"
+    t.string    "address"
+    t.float     "latitude"
+    t.float     "longitude"
+    t.boolean   "is_merchant",            :default => false
+    t.string    "description"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
